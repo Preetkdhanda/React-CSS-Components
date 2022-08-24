7883baecbc43486fce941c16a023123889b9d615
Harpreet Dhanda <harpreet_h@hotmail.co.uk>
2022-08-24 10:55:55 +0100

    CSS with react components

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+import LaunchContainer from "./containers/LaunchContainer";
+
+function App() { //APP IS THE BODY OF THE PAGE FOR CSS!!
+  return (
+    <LaunchContainer />
+  )
+}
+
+export default App;
---
+++ src/components/LaunchDetails.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+
+const LaunchDetails = ({launch}) => {
+
+  if (!launch){
+    return <p>Loading...</p>
+  }
+
+  const Paragraph = styled.p`
+  
+  margin: 0;
+  padding: 1em;
+  background-color: #ced7e0;
+  color: #062c43
+
+  `;
+
+  const BigParagraph = styled(Paragraph)`
+
+  font-size: 1.5em;
+  padding: .5em;
+  border-bottom: 1px solid #062c43;
+  
+
+  `;
+
+  return (
+    <>
+      <BigParagraph>{launch.mission_name}</BigParagraph>
+      <Paragraph>Flight Number: {launch.flight_number}</Paragraph>
+      <Paragraph>Year: {launch.launch_year}</Paragraph>
+      <Paragraph>Rocket: {launch.rocket.rocket_name}</Paragraph>
+      <Paragraph>Details of launch: {launch.details}</Paragraph>
+    </>
+  )
+
+}
+
+export default LaunchDetails;
